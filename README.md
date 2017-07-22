730f62159cb410646db77f7b3e29eef89965f06b
Aaron Benson <aaro9237@esri.com>
2017-07-22 09:15:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# Overview
+ArcGIS Runtime SDK for Android v100.1.0 samples.  The `master` branch of this repository contains sample app modules for the latest available version of the [ArcGIS Runtime SDK for Android](https://developers.arcgis.com/android/). Samples released under older versions can be found through the [git tags](https://github.com/Esri/arcgis-runtime-samples-android/tags).  Please read our [wiki](https://github.com/Esri/arcgis-runtime-samples-android/wiki) for help with working with this repository.  
+
+# Prerequisites
+* The samples are building with `compileSdkVersion 25` which requires [JDK 7 or higher](http://www.oracle.com/technetwork/java/javase/downloads/index.html)
+* [Android Studio](http://developer.android.com/sdk/index.html)
+
+## SDK Maven repo
+The SDK is available through the Bintray Maven repo, you can take a look at the repository with the link below: 
+
+[ ![Download](https://api.bintray.com/packages/esri/arcgis/arcgis-android/images/download.svg?version=100.1.0) ](https://bintray.com/esri/arcgis/arcgis-android/100.1.0/link)
+
+## Developer Instructions
+Please read our [developer instructions wiki page](https://github.com/Esri/arcgis-runtime-samples-android/wiki/dev-instructions) to set up your developer enviroment with Android Studio.  Instructions include forking and cloning the repository for those new to Git
+
+## Run a sample
+Once you have set up your developer environment you can run any sample from within Android Studio by selecting the app module from the **Edit Configurations** drop down and clicking the **Run** button from the toolbar. 
+
+### Build/Run sample from Gradle
+You can execute all the build tasks using the [Gradle Wrapper](https://docs.gradle.org/current/userguide/gradle_wrapper.html) command line tool. It's available as a batch file for Windows (gradlew.bat) and a shell script for Linux/Mac (gradlew.sh) and it is accessible from the root of the project.  
+
+- Build a debug APK
+
+```
+$ ./gradlew assembleDebug
+```
+
+- Run the app
+
+**Device**
+```
+adb -d install path/to/sample.apk
+```
+
+Built APK's are saved to **arcgis-runtime-samples-android/[module-name]/build/outputs/apk/**. More information about running apps on devices can be found [here](https://developer.android.com/studio/run/device.html).
+
+## Issues
+Find a bug or want to request a new feature enhancement?  Please let us know by submitting an issue.
+
+## Contributing
+Anyone and everyone is welcome to [contribute](.github/CONTRIBUTING.md). We do accept pull requests.
+
+1. Get Involved
+2. Report Issues
+3. Contribute Code
+4. Improve Documentation
+
+Please see our [guidelines for contributing doc](https://github.com/Esri/contributing/blob/master/README.md)
+
+## Licensing
+Copyright 2017 Esri
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+A copy of the license is available in the repository's [license.txt](https://github.com/Esri/arcgis-android-sdk-gradle-samples/blob/master/LICENSE) file.
+
+[](Esri Tags: ArcGIS Android Mobile)
+[](Esri Language: Java)​
---
+++ show-callout/src/main/java/com/esri/arcgisruntime/samples/showcallout/MainActivity.java
@@ -0,0 +1,230 @@
+/* Copyright 2016 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.esri.arcgisruntime.samples.showcallout;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+import com.esri.arcgisruntime.concurrent.ListenableFuture;
+import com.esri.arcgisruntime.data.Feature;
+import com.esri.arcgisruntime.data.FeatureQueryResult;
+import com.esri.arcgisruntime.data.Field;
+import com.esri.arcgisruntime.data.QueryParameters;
+import com.esri.arcgisruntime.data.ServiceFeatureTable;
+import com.esri.arcgisruntime.geometry.Envelope;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.SpatialReference;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
+import com.esri.arcgisruntime.layers.FeatureLayer;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.Callout;
+import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.mapping.view.LocationDisplay;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String sTag = "Gesture";
+    private MapView mMapView;
+    private Callout mCallout;
+
+    private final SpatialReference wgs84 = SpatialReference.create(4326);
+
+
+    private int requestCode = 2;
+    String[] reqPermissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission
+            .ACCESS_COARSE_LOCATION};
+
+    private class DownloadWebPageTask extends AsyncTask<String, Void, double[]> {
+        @Override
+        protected double[] doInBackground(String... urls) {
+            double[] response = new double[2];
+            String htmlContent = "";
+            for (String url : urls) {
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet httpGet = new HttpGet(url);
+                try {
+                    HttpResponse execute = client.execute(httpGet);
+                    InputStream content = execute.getEntity().getContent();
+
+                    BufferedReader buffer = new BufferedReader(
+                            new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        htmlContent += s;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            int beginIndexX = htmlContent.indexOf("XMin: ") + 6;
+            int endIndexX = beginIndexX + 1;
+            while (Character.isDigit(htmlContent.charAt(endIndexX)) || htmlContent.charAt(endIndexX) == '-' || htmlContent.charAt(endIndexX) == '.') {endIndexX++;}
+            int beginIndexY = htmlContent.indexOf("YMin: ") + 6;
+            int endIndexY = beginIndexY + 1;
+            while (Character.isDigit(htmlContent.charAt(endIndexY)) || htmlContent.charAt(endIndexY) == '-' || htmlContent.charAt(endIndexY) == '.') {endIndexY++;}
+            return new double[] { Double.parseDouble(htmlContent.substring(beginIndexX, endIndexX)), Double.parseDouble(htmlContent.substring(beginIndexY, endIndexY)) };
+        }
+        @Override
+        protected void onPostExecute(double[] result) {
+            int hi = 1;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // inflate MapView from layout
+        mMapView = (MapView) findViewById(R.id.mapView);
+        // create a map with the Basemap Type topographic
+        final ArcGISMap mMap = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.0565, -117.195800, 18);
+        //mMap = new ArcGISMap(Basemap.createImagery());
+        // set the map to be displayed in this view
+        mMapView.setMap(mMap);
+
+        // If an error is found, handle the failure to start.
+        // Check permissions to see if failure may be due to lack of permissions.
+        boolean permissionCheck1 = ContextCompat.checkSelfPermission(MainActivity.this, reqPermissions[0]) ==
+                PackageManager.PERMISSION_GRANTED;
+        boolean permissionCheck2 = ContextCompat.checkSelfPermission(MainActivity.this, reqPermissions[1]) ==
+                PackageManager.PERMISSION_GRANTED;
+
+        if (!(permissionCheck1 && permissionCheck2)) {
+            // If permissions are not already granted, request permission from the user.
+            ActivityCompat.requestPermissions(MainActivity.this, reqPermissions, requestCode);
+        }
+
+        final ServiceFeatureTable serviceFeatureTable = new ServiceFeatureTable("https://services8.arcgis.com/DnMrXNZ4mQTjDjkz/ArcGIS/rest/services/Unicorn/FeatureServer/0");
+        final FeatureLayer mFeatureLayer = new FeatureLayer(serviceFeatureTable);
+        mMap.getOperationalLayers().add(mFeatureLayer);
+
+        final Handler h = new Handler();
+        final int delay = 3000; //milliseconds
+
+        h.postDelayed(new Runnable(){
+            public void run(){
+                // Get user location
+                mMapView.getLocationDisplay().setAutoPanMode(LocationDisplay.AutoPanMode.RECENTER);
+                if (!mMapView.getLocationDisplay().isStarted())
+                    mMapView.getLocationDisplay().startAsync();
+                double userXLoc = mMapView.getLocationDisplay().getLocation().getPosition().getX();
+                double userYLoc = mMapView.getLocationDisplay().getLocation().getPosition().getY();
+
+                readWebpage("https://services8.arcgis.com/DnMrXNZ4mQTjDjkz/ArcGIS/rest/services/Unicorn/FeatureServer/0");
+
+                // Get unicorn location from server
+                /*while (serviceFeatureTable.getExtent() == null) {}
+                SpatialReference map = mFeatureLayer.getSpatialReference();
+                double unicornXLoc = mFeatureLayer.getFullExtent().getXMin() / (111.325 * 1000);
+                double unicornYLoc = mFeatureLayer.getFullExtent().getYMax() / (111.325 * 1000);
+                mMap.getOperationalLayers().remove(mMap.getOperationalLayers().size() - 1);
+                final ServiceFeatureTable serviceFeatureTable = new ServiceFeatureTable("https://services8.arcgis.com/DnMrXNZ4mQTjDjkz/ArcGIS/rest/services/Unicorn/FeatureServer/0");
+                final FeatureLayer mFeatureLayer = new FeatureLayer(serviceFeatureTable);
+                mMap.getOperationalLayers().add(mFeatureLayer);*/
+
+                // Check distance
+                //double distance = distanceCheckUnicorn(userXLoc, userYLoc, unicornXLoc, unicornYLoc);
+
+                // Upload change to unicorn location and alert user if found
+
+
+
+                h.postDelayed(this, delay);
+            }
+        }, delay);
+    }
+
+
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mMapView.pause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mMapView.resume();
+    }
+
+
+
+    private double distanceCheckUnicorn(double latUser, double longUser, double latUnicorn, double longUnicorn) {
+        double distance = Math.sqrt(Math.pow(latUnicorn - latUser, 2) + Math.pow(longUnicorn - longUser, 2));
+        distance *= 111.325f * 1000; //Gets distance in meters
+        return distance;
+    }
+
+    public void readWebpage(String webAddress) {
+        DownloadWebPageTask task = new DownloadWebPageTask();
+        task.execute(new String[] { webAddress });
+    }
+
+}
+
+/*
+    android.graphics.Point screenPoint = new android.graphics.Point(Math.round(motionEvent.getX()),
+            Math.round(motionEvent.getY()));
+    // create a map point from screen point
+    Point mapPoint = mMapView.screenToLocation(screenPoint);
+    // convert to WGS84 for lat/lon format
+    Point wgs84Point = (Point) GeometryEngine.project(mapPoint, SpatialReferences.getWgs84());
+    // create a textview for the callout
+    TextView calloutContent = new TextView(getApplicationContext());
+                calloutContent.setTextColor(Color.BLACK);
+                        calloutContent.setSingleLine();
+                        // format coordinates to 4 decimal places
+                        calloutContent.setText("Lat: " +  String.format("%.4f", wgs84Point.getY()) +
+                        ", Lon: " + String.format("%.4f", wgs84Point.getX()));
+
+                        // get callout, set content and show
+                        mCallout = mMapView.getCallout();
+                        mCallout.setLocation(mapPoint);
+                        mCallout.setContent(calloutContent);
+                        mCallout.show();*/
